2945adde86dff225e8c996859e5b67b734aabe89
John Mikhail <johnpmikhail@gmail.com>
2024-09-10 21:21:03 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,10 +11,12 @@ appropriate 'values' vector.
 import numpy as np
 from Dimension import Dimension
     
-## Fundamental dimensions
-# if more are needed, simply append to the list of labels. However, user should
-#   ensure the included dimensions are mutually independent
-labels = ['Mass', 'Length', 'Time', 'Temperature', 'Current', \
+## Fundamental Dimensions
+'''
+If more are needed, simply append to the list of labels. However, user should
+ensure the included dimensions are mutually independent.
+'''
+labels = ['Mass', 'Length', 'Time', 'Temperature', 'E_Current', \
           'Luminous_Intensity', 'Quantity']
 n_dims = len(labels)
 
@@ -32,33 +34,55 @@ Mass        = fundamental_dims[labels['Mass']]
 Length      = fundamental_dims[labels['Length']]
 Time        = fundamental_dims[labels['Time']]
 Temperature = fundamental_dims[labels['Temperature']]
-Current     = fundamental_dims[labels['Current']]
+# note: E_ prefix used as shorthand for "Electric_"
+E_Current   = fundamental_dims[labels['E_Current']]
+'''
+note that Quantity can be set equal to Dimensionless if it is not needed
+for disambiguation
+'''
 Quantity    = fundamental_dims[labels['Quantity']]
 
-## Derived dimensions
-Velocity = Length/Time
-Acceleration = Velocity/Time
-Area = Length**2
-Volume = Length**3
-Force = Mass*Acceleration
-Energy = Force*Length
-Pressure = Force/Area # also stress or bulk modulus
-Compressibility = Pressure**(-1)
-Density = Mass/Volume
-Number_Density = Volume**(-1)
-Frequency = Time**(-1)
-Surface_Tension = Force/Length
-Wavenumber = Length**(-1)
-Charge = Current/Time
-Voltage = Energy/Charge
-E_Field = Force/Charge
-B_Field = Force/Charge/Velocity
-Dynamic_Viscosity = Pressure*Time
-Kinematic_Viscosity = Dynamic_Viscosity/Density
-Thermal_Expansion = Temperature**(-1)
+## Derived Dimensions
+### derived from Fundamental Dimensions
+Velocity               = Length/Time
+Area                   = Length**2
+Volume                 = Length**3
+Frequency              = Time**(-1)
+Wavenumber             = Length**(-1)
+Charge                 = E_Current*Time
+Thermal_Expansion      = Temperature**(-1)
 
-## Physical constants
-Planck_const = Energy*Time
-# heat capacity, entropy
-Boltzmann_const = Energy/Temperature
-Ideal_gas_const = Energy/Temperature/Quantity
+### derived from other Derived Dimensions
+Acceleration           = Velocity/Time
+Force                  = Mass*Acceleration
+Energy                 = Force*Length
+Power                  = Energy/Time
+Pressure               = Force/Area # == stress, bulk modulus
+Compressibility        = Pressure**(-1)
+Density                = Mass/Volume
+Number_Density         = Quantity/Volume
+Surface_Tension        = Force/Length
+
+Voltage                = Energy/Charge
+E_Resistance           = Voltage/E_Current
+E_Conductance          = E_Resistance**(-1)
+Capacitance            = Charge/Voltage
+E_Field                = Force/Charge
+B_Field                = Force/Charge/Velocity
+Magnetic_Permeability  = Force/E_Current**2
+H_Field                = B_field/Magnetic_Permeability
+E_Inductance           = Energy/E_Current**2
+Permittivity           = Capacitance/Length
+
+Dynamic_Viscosity      = Pressure*Time
+Kinematic_Viscosity    = Dynamic_Viscosity/Density
+
+## Physical Constants
+Planck_const           = Energy*Time
+Boltzmann_const        = Energy/Temperature # == heat capacity, entropy
+Avogadro_const         = Quantity**(-1)
+Ideal_gas_const        = Energy/Temperature/Quantity
+Gravitational_const    = Force*Area/Mass**2
+Coulomb_const          = Force*Area/Charge**2
+Stefan_Boltzmann_const = Power/Area/Temperature**4
+Cosmological_const     = Area**(-1)
